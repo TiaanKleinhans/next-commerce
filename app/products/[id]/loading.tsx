0ad9828ca609f648534ba544b1c6efa233b164e6
TiaanKleinhans <Kleinhanstiaan89@gmail.com>
2025-10-23 20:35:09 +0200

    404-page

--- app/products/[id]/loading.tsx
+++ app/products/[id]/loading.tsx
@@ -1,37 +1,26 @@
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { getProductsById } from '@/lib/actions/products-actions';
 import { formatPrice } from '@/lib/utils/format-price';
 import { Separator } from '@radix-ui/react-separator';
 import { notFound } from 'next/navigation';
 
-export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  const product = await getProductsById(id);
-
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  if (!product) {
-    notFound();
-  }
-
+export default async function Loading() {
   return (
     <main className="container mx-auto p-4">
       <Card className="max-w-3xl mx-auto">
         <CardContent className="p-6">
-          <h1 className="text-3xl font-bold mb-6">{product.name}</h1>
+          <Skeleton className="h-8 w-1/2" />
 
           <div className="flex item-center gap-2 mb-4">
-            <span className="font-semibold text-lg">{formatPrice(product.sellingPrice)}</span>
-
-            <Badge variant="outline">{product.category?.name}</Badge>
+            <Skeleton className="h-8 w-1/2" />
+            <Skeleton className="h-8 w-1/2" />
           </div>
           <Separator className="my-4" />
 
           <div className="space-y-2">
-            <h2 className="font-medium">
-              <p>{product.description}</p>
-            </h2>
+            <Skeleton className="h-8 w-1/2" />
           </div>
         </CardContent>
       </Card>
--- lib/actions/products-actions.ts
+++ lib/actions/products-actions.ts
@@ -5,6 +5,9 @@ import { prisma } from '../prisma';
 export async function getProductsById(id: string) {
   const product = await prisma.productModel.findUnique({
     where: { id },
+    include: {
+      category: true,
+    },
   });
 
   return !product ? null : product;
